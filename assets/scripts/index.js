7a20bbe69dbe1de70ddaa21ce3875cb31227ac66
Fadela Numah Kadenza <fadelanumah@gmail.com>
2022-09-19 14:33:00 +0700

    modified css and html structure

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const navLogo = document.querySelector('.logo-nav');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('active');
+//     navMenu.classList.toggle('active');
+// })
 
-document.querySelector('.nav-link').forEach(n => n.addEventListener('click', ()=> {
-    hamburger.classList.remove('active');
-    hamburger.classList.remove('active');
-}))
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navLogo.classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-link").forEach((item) =>
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLogo.classList.remove("active");
+        navMenu.classList.remove('active');
+    })
+);
